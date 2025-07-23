21c4054c4b5a3a1257cc949b53be0c65265677bc
1ErnestoMokwena <106978270+1ErnestoMokwena@users.noreply.github.com>
2025-07-23 06:17:37 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -7,10 +7,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyBnN7kl9SI00cWNvu5tGHxOJJcODcyc4a4",
   authDomain: "kwenaworld-marketplace.firebaseapp.com",
   projectId: "kwenaworld-marketplace",
-  storageBucket: "kwenaworld-marketplace.appspot.com",
+  storageBucket: "kwenaworld-marketplace.appspot.com",  // ✅ Fixed here
   messagingSenderId: "906613354644",
   appId: "1:906613354644:web:039527718d1a4788ac4481",
   measurementId: "G-PHQ57NC75Q"
+};
 };
 
 const app = initializeApp(firebaseConfig);
