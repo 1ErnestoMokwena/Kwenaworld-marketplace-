51484557805d2da697738e1937d22838a4fb251f
1ErnestoMokwena <106978270+1ErnestoMokwena@users.noreply.github.com>
2025-07-25 20:31:42 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,9 +4,9 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnN7kl9SI00cWNvu5tGHxOJJcODcyc4a4",
-  authDomain: "kwenaworld-marketplace.firebaseapp.com",
-  projectId: "kwenaworld-marketplace",
-  storageBucket: "kwenaworld-marketplace.appspot.com",
+  authDomain: "questionmarkt-online.firebaseapp.com",
+  projectId: "questionmarkt",
+  storageBucket: "questionmarkt-online.appspot.com",
   messagingSenderId: "906613354644",
   appId: "1:906613354644:web:039527718d1a4788ac4481",
   measurementId: "G-PHQ57NC75Q"
